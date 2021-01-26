0d96a1fd08df943fd3df392182991b3e0e2c5d19
timdancan <saqe789@gmail.com>
2021-01-26 12:53:26 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './index.css'
+const numbers = [2, 3, 5];
+
+// Стрелочная функция. Не запнётся ли на ней Internet Explorer?
+const doubledNumbers = numbers.map(number => number * 2);
+
+console.log(doubledNumbers);
